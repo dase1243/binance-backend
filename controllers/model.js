0e8d3c059da8f05436ebda06c6924ce7c87bad62
Andrei <dreiglushko@gmail.com>
2021-10-30 20:43:06 +0400

    fix

--- controllers/model.js
+++ controllers/model.js
@@ -51,7 +51,7 @@ exports.create = async (req, res) => {
         console.log("req.files: ", req.files)
         console.log("req.body: ", req.body)
 
-        const fileName = saveFile(req.params.image);
+        const fileName = saveFile(req.files.image);
 
         const bitmap = fs.readFileSync(fileName);
 
