50aa38c92e0a8fd73c4df486f04cb841fe14e4a0
Andrei <dreiglushko@gmail.com>
2021-10-30 16:43:56 +0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const userRoutes = require('./user');
 const modelRoutes = require('./model');
 
 function routes(app) {
-  app.use('/api/user', userRoutes);
+  app.use('/api/user/', userRoutes);
   app.use('/api/model/', modelRoutes);
 }
 
